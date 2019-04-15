7ee72d03866f4b0a7cffc8a03318b6597f2c2135
James Declerus <james.declerus@esih.edu>
2019-04-15 15:51:12 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,7 +1,7 @@
 # Write your code here.
 def badge_maker(name)
   return "Hello, my name is #{name}."
-end 
+end
 
 def batch_badge_creator(attendees)
   names=[]
@@ -9,23 +9,23 @@ def batch_badge_creator(attendees)
   badges.push("Hello, my name is #{name}.")
   end
   return badges
-  
-end 
+
+end
 
 def assign_rooms(speakers)
   assign=[]
-  
+
   speakers.each_with_index{|speaker,index|
   assign.push("Hello, #{speaker}! You'll be assigned to room #{index + 1}!")
     }
    return assign
-end 
+end
 
 def printer(attendees)
   batch_badge_creator(attendees).each do |badge|
     puts badge
   end
-  
+
   assign_rooms(attendees).each do |badge|
     puts badge
   end
